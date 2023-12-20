0ef1b7c0bbeb60f6c4fd3332d7b162932828cb02
pdsatter <87093940+pdsatter@users.noreply.github.com>
2023-12-20 13:58:20 -0600

    Add accordion to hobby section

--- portfolio-app/src/Hobbies.tsx
+++ portfolio-app/src/Hobbies.tsx
@@ -1,42 +1,72 @@
 import { Carousel } from "react-bootstrap";
+import { Accordion } from "react-bootstrap";
+
+function WrestlingCarousel() {
+    return(
+        <Carousel>
+            <Carousel.Item>
+                <img
+                className="photo d-block"
+                src={require("./photos/Wrestling1.jpg")}
+                alt="Photo 1"
+                ></img>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="photo d-block"
+                src={require("./photos/Wrestling2.jpg")}
+                alt="Photo 2"
+                ></img>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="photo d-block"
+                src={require("./photos/Trophy.jpg")}
+                alt="Photo 3"
+                ></img>
+            </Carousel.Item>
+        </Carousel>
+    )
+    
+}
 
 function Hobbies() {
     return (
         <div>
             <h2><strong>Hobbies</strong></h2>
-            <h4>Wrestling</h4>
-            <p>
-                I was a varsity wrestler in high school, and <strong>wrestling</strong> is my favorite sport by far.
-                I am on my journey of learning other martial arts such as muay thai and judo.
-            </p>
+            <Accordion defaultActiveKey="0">
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>Wrestling</Accordion.Header>
+                    <Accordion.Body>
+                        <p>
+                            I was a varsity wrestler in high school, and <strong>wrestling</strong> is my favorite sport by far.
+                            I am on my journey of learning other martial arts such as muay thai and judo.
+                        </p>
+                        <WrestlingCarousel />
 
-            <Carousel>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src={require("./photos/Wrestling1.jpg")}
-                    alt="First slide"
-                    ></img>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src="https://lorempixel.com/800/400/food/1"
-                    alt="First slide"
-                    ></img>
-                </Carousel.Item>
-            </Carousel>
+                    </Accordion.Body>
+                </Accordion.Item>
 
-            <h4>Juggling</h4>
-            <p>
-                I love learning new skills. One interesting skill I learned is <strong>juggling</strong>.
-                Juggling takes a lot of work to learn, but learning the tricks is worth it in the end.
-            </p>
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header>Juggling</Accordion.Header>
+                    <Accordion.Body>
+                        <p>
+                            I love learning new skills. One interesting skill I learned is <strong>juggling</strong>.
+                            Juggling takes a lot of work to learn, but learning the tricks is worth it in the end.
+                        </p>
+                    </Accordion.Body>
+                </Accordion.Item>
 
-            <h4>Gaming</h4>
-            <p>
-                I also love to game in my free time.  Some of my favorite games are <strong>Rocket League, CS2, Super Smash Bros, and Rainbow Six Siege</strong>.
-            </p>
+                <Accordion.Item eventKey="2">
+                    <Accordion.Header>Gaming</Accordion.Header>
+                    <Accordion.Body>
+                        <p>
+                            I also love to game in my free time.  Some of my favorite games are <strong>Rocket League, CS2, Super Smash Bros, and Rainbow Six Siege</strong>.
+                        </p>
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
+            
         </div>
     );
 }
