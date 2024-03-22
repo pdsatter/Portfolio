6267fb7156f4af316c20d25b1cecc75cec7e62e2
pdsatter <87093940+pdsatter@users.noreply.github.com>
2024-03-22 08:47:37 -0500

    Upload new resume

--- portfolio-app/src/Resume.tsx
+++ portfolio-app/src/Resume.tsx
@@ -1,7 +1,7 @@
 function Resume() {
     return (
         <div className="resume Sticky-Resume">
-            <a href="resume.pdf" download><strong><span>Download Resume</span></strong></a>
+            <a href="resume-preston-satterfield.pdf" download><strong><span>Download Resume</span></strong></a>
         </div>
     )
 }
